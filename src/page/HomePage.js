65dab3d873f66053a4291111a2fd6e0f14d3af30
Romain CAILLAT <caillat.romain@epitech.eu>
2023-06-21 14:12:52 +0200

    🐛 fix text bug nextui

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -16,13 +16,12 @@ function IntroPage() {
         
         />
         <div className="blur-overlay" style={{
-          position: "absolute",
           top: "0",
           left: "0",
           width: "100%",
           height: "100%",
           backgroundColor: "rgba(0, 0, 0, 0.5)"}}/>
-        <Text h1 size={50} style={{ 
+        <h1 size={50} style={{ 
           position: "absolute", 
           left: "50%", 
           top: "50%", 
@@ -30,7 +29,7 @@ function IntroPage() {
           transform: "translate(-50%, -50%)", 
           color: "white",}}>
           RÉINVENTONS NOTRE MODE DE VIE ET FAISONS ÉVOLUER NOTRE VISION DE L'HABITAT
-        </Text>
+        </h1>
     </div>
   )
 }
