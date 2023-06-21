ffad3c4f3b4a2899150bafacb4c68e50607193a2
Romain CAILLAT <caillat.romain@epitech.eu>
2023-06-21 14:12:00 +0200

    🚀 deploy react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import NavbarComponent from "./Component/Navbar";
 import MainPresentation from "./page/HomePage"
+import ContactPage from "./page/ContactPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavbarComponent/>
-      </header>
-      <main>
-        <MainPresentation/>
-      </main>
+    <div className="App" >
+      <BrowserRouter>
+        {/* <header className="App-header"> */}
+          <NavbarComponent/>
+        {/* </header> */}
+        {/* <main> */}
+          <Routes>
+            <Route element={<MainPresentation/>} path="/"/>
+            <Route element={<ContactPage/>} path="/contact"/>
+          </Routes>
+        {/* </main> */}
+      </BrowserRouter>
     </div>
   );
 }
