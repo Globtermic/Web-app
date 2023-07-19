b50e6032845a27a3fe678b841ec5ff1f96a0de3e
nicolas humbert <nicolas.humbert@epitech.eu>
2023-07-19 14:50:26 +0200

    :lipstick: add new image for the contact page

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -143,7 +143,6 @@ export default function MainPresentation() {
       <PresentationRenovationAsset/>
       <PresentationAmenagementAsset/>
       <Spacer y={3}/>
-      <EndPage/>
     </div>
   )
 }
