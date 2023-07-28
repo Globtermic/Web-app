2d0fbe3e32b714994a7ce74f057f28117c7b9725
nicolas humbert <nicolas.humbert@epitech.eu>
2023-07-28 14:45:51 +0200

    :lipstick: add style when hover buttons navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -3,6 +3,7 @@ import { Navbar, Button } from '@nextui-org/react'
 import logo from '../assets/Logo_Globtermic.png'
 import { Link, useNavigate } from "react-router-dom"
 import { useTranslation } from 'react-i18next'
+import './Styles/Navbar.css'
 
 export default function NavbarComponent() {
     const {t, i18n} = useTranslation();
@@ -16,25 +17,25 @@ export default function NavbarComponent() {
 
     return (
 
-            <Navbar variant="sticky" maxWidth={"fluid"} isBordered >
+            <Navbar variant="sticky" height={'90px'} maxWidth={"fluid"} isBordered >
                 <Navbar.Brand>
                     <img src={logo} alt="logo of compagnie" style={{ width: "40px", marginLeft: "10px"}}/>
                 </Navbar.Brand>
                 <Navbar.Toggle showIn={'xs'}/>
                 <Navbar.Content gap={'$5'} hideIn="xs">
-                    <Button style={{height: "100%"}} light auto onPress={() => goto("/")} ripple={false}  >
+                    <Button style={{height: "100%"}} className='buttonStyle' light auto onPress={() => goto("/")} ripple={false}  >
                         {t('navbar:homePage')}
                     </Button>
-                    <Button style={{height: "100%"}} light auto onPress={() =>goto("/modeles")} ripple={false}>
+                    <Button style={{height: "100%"}} className='buttonStyle' light auto onPress={() =>goto("/modeles")} ripple={false}>
                     {t('navbar:ourModels')}
                     </Button>
-                    <Button style={{height: "100%"}} light auto ripple={false} onPress={() => window.location.href='https://www.seloger.com/professionnels/constructeurs/paris-12eme-arrondissement-75012/agence-322743/'}>
+                    <Button style={{height: "100%"}} className='buttonStyle' light auto ripple={false} onPress={() => window.location.href='https://www.seloger.com/professionnels/constructeurs/paris-12eme-arrondissement-75012/agence-322743/'}>
                     {t('navbar:ourArticles')}
                     </Button>
-                    <Button style={{height: "100%"}} light auto ripple={false}>
+                    <Button style={{height: "100%"}} className='buttonStyle' light auto ripple={false}>
                     {t('navbar:aboutUs')}
                     </Button>
-                    <Button style={{height: "100%"}} light auto onPress={() => goto("/contact")} ripple={false}>
+                    <Button style={{height: "100%"}} className='buttonStyle' light auto onPress={() => goto("/contact")} ripple={false}>
                     {t('navbar:contact')}
                     </Button>
                 </Navbar.Content>
