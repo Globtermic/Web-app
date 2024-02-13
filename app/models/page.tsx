da35f0719fda810f5eb319e2d650d118bffafc65
Nicolas Humbert <nemarrejean9@gmail.com>
2024-02-13 17:30:27 +0100

    :rocket: add struct for house data and serialization

--- app/models/page.tsx
+++ app/models/page.tsx
@@ -2,6 +2,19 @@ import { title } from "@/components/primitives";
 import {Card} from "@nextui-org/card";
 import {Button, ButtonGroup} from "@nextui-org/button";
 import {Image} from "@nextui-org/image";
+import './page.css';
+
+interface HouseData {
+	name: string,
+	price: number,
+	attributes: string[],
+}
+
+const data: HouseData = {
+	name: 'Unidad',
+	price: 200000,
+	attributes: ["150m²", "Jardin", "Garage"]
+}
 
 const ArrowIcon = () => {
     return (
@@ -15,45 +28,60 @@ const ArrowIcon = () => {
     )
 }
 
-function AttributeCard({name}) {
-	return <Card className="p-2 shadow-none bg-green-600 ml-4" >
-		<h2 className="text-white font-light">{name}</h2>
+const FormIcon = () => {
+	return (<svg viewBox="0 -6 48 48" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path id="letter" d="M317,337a5.005,5.005,0,0,1-5-5V306a4.975,4.975,0,0,1,1-3h0a5,5,0,0,1,4-2h38a5,5,0,0,1,4,2h0a4.974,4.974,0,0,1,1,3v26a5.005,5.005,0,0,1-5,5Zm0-2h38a2.984,2.984,0,0,0,1.738-.556l-14.721-14.721-3.129,3.08a4.129,4.129,0,0,1-5.775,0l-3.129-3.081-14.721,14.721A2.984,2.984,0,0,0,317,335Zm-3-29v26a2.984,2.984,0,0,0,.555,1.737l14.716-14.715-14.825-14.6A2.982,2.982,0,0,0,314,306Zm43.445,27.737A2.983,2.983,0,0,0,358,332V306a2.981,2.981,0,0,0-.447-1.573l-14.825,14.6Zm-23.61-11.644a3.1,3.1,0,0,0,4.332,0l18.717-18.427A2.987,2.987,0,0,0,355,303H317a2.986,2.986,0,0,0-1.883.666Z" transform="translate(-312 -301)" fill="#000000"></path> </g></svg>);
+}
+
+
+function AttributeCard({name} : {name: string}) {
+	return <Card className="house-card-attribute-card " >
+		<h2 className="">{name}</h2>
 	</Card>
 }
 
-function HouseCard() {
-	return <Card className=" flex items-center rounded-none h-96" >
+
+
+function AttributeCardList({names} : {names: string[]}) {
+	return (<div className="house-card-attribute-specs">
+		{names.map((name, index) => {return (<AttributeCard key={index} name={name}/>)})}
+	</div>)
+}
+
+function HouseCard({data} : {data: HouseData}) {
+	return <div className="house-card ">
+	<Card className="house-card-custom flex items-center rounded-none " >
 		<Image alt="unidad" src={"/images/unidad.jpg"} className=" h-45 rounded-none" />
-		<Card className="flex-row items-center justify-between mt-3 w-full border-none shadow-none">
-			<div className="ml-2">
-				<h1 className="text-xl font-med">UNIDAD</h1>
-				
+		<Card className="house-card-attribute">
+			<div className="house-card-name">
+				<h1>{data.name}</h1>
 			</div>
-			<h2 className="text-lg mr-2">200 000€</h2>
-		</Card>
-		
-		<Card className="flex-row items-center justify-normal mt-3 w-full h-30 border-none shadow-none ml-2">
-			<AttributeCard name={"150m²"}></AttributeCard>
-			<AttributeCard name={"Jardin"}></AttributeCard>
-			<AttributeCard name={"Garage"}></AttributeCard>
-		</Card>
-		<Card className="flex-row items-center justify-between mt-4 w-full border-none rounded-none shadow-none end-0" >
-			<div className="ml-2 flex items-center">
-				<Button className="flex items-center w-28 rounded-none hover:bg-gray-200 hover:border-solid hover:border-gray-700 hover:border-1" endContent={<ArrowIcon/>} >Nous Contacter</Button>
+			<div className="house-card-attribute-specs">
+				{/* <AttributeCard name="150m²"/>
+				<AttributeCard name="Jardin"></AttributeCard>
+			<AttributeCard name="Garage"></AttributeCard> */}
+			<AttributeCardList names={data.attributes} />
 			</div>
-			<div className="mr-2 flex items-center">
-				<Button className="items-center w-28 rounded-none hover:bg-gray-200 hover:border-solid hover:border-gray-700 hover:border-1" endContent={<ArrowIcon/>} >Voir</Button>
+			<div className="house-card-contact-button">
+				<FormIcon/>
+			</div>
+			<div className="house-card-price">
+			<h2 className="">{data.price} €</h2>
 			</div>
 		</Card>
+
 	</Card>
+	</div> 
 }
 
 export default function AboutPage() {
 	return (
-		<div className="grid grid-cols-3 gap-6 w-full">
-			<HouseCard/>
-			<HouseCard/>
-			<HouseCard/>
+		<div className="page-grid">
+			<HouseCard data={data}/>
+			<HouseCard data={data}/>
+			<HouseCard data={data}/>
+			<HouseCard data={data}/>
+			<HouseCard data={data}/>
+			<HouseCard data={data}/>
 		</div>
 	);
 }
